ef14621833affe45a9f1bd7b0a3d35617435c7d0
Brandon <brandonkargus@gmail.com>
2021-09-29 10:25:49 -0500

    all labs from "fundamentals" folder

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,30 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
+        int i = 2147483647;
         System.out.println("int i is: " + i);
 
-        // write your code below
+        long l = 9223372036854775807L;
+        System.out.println("long l is: " + l);
+
+        byte b = 127;
+        System.out.println("byte b is: " + b);
+
+        short s = 32767;
+        System.out.println("short s is: " + s);
+
+        float f = 1.73463636373f;
+        System.out.println("float f is: " + f);
+
+        double d = 1.6464738393937383;
+        System.out.println("double d is: " + d);
+
+        char c = 'c';
+        System.out.println("char c is: " + c);
+
+        boolean t = true;
+        System.out.println("boolean t is: " + t);
+
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,22 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int stringLength = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
 
+        boolean same = str.length() == str2.length();
         // please concatenate str & str2 and set the result to a new String variable below
+
+        String str3 = str + str2;
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+        String str4 = str3.replaceAll("[^0-9]", "#");
+        int itsHere = str.indexOf('!');
+        String notAgoodPlace = str.substring(0, 3);
 
     }
 
