9dd0697db24c1337851a05b8601ebcd395911027
Brandon <brandonkargus@gmail.com>
2021-11-02 12:17:43 -0500

    I/O exercise 2, finalized

--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -10,7 +10,7 @@ import java.io.*;
  * -Make sure you close the connections to both files.             (COMPLETE, see "encryptionExample.txt")
  * <p>
  * Then, read back the encrypted file using the BufferedReader and
- * print out the unencrypted version. Does it match the original file?           (YES, except for "￿￿" added at very end after last sentence)
+ * print out the unencrypted version. Does it match the original file?           (YES)
  */
 
 public class Exercise_02 {
