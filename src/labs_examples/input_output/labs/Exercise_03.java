d4a3e0c88af4a13a3a581417575bd0cf19db2bc7
Brandon <brandonkargus@gmail.com>
2021-11-02 13:38:43 -0500

    I/O Exercise_03 complete

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -5,13 +5,45 @@ import java.io.*;
 /**
  * Input/Output Exercise 3: variety
  * <p>
- * 1) Demonstrate the usage of at least two additional Byte Streams         (USED ByteArrayInputStream, and ByteArrayOutputStream)
- * 2) Demonstrate the usage of at least two additional Character Streams    (USED StringWriter, and StringReader)
- * 3) Demonstrate using a buffer on one of the Byte Streams and one of the Character Streams (USED StringBuffer on StringWriter, and....
- * 4) Demonstrate the use of the DataInputStream and DataOutputStream
+ * 1) Demonstrate the usage of at least two additional Byte Streams         (DONE ByteArrayInputStream, and ByteArrayOutputStream)
+ * 2) Demonstrate the usage of at least two additional Character Streams    (DONE StringWriter, and StringReader)
+ * 3) Demonstrate using a buffer on one of the Byte Streams and one of the Character Streams (DONE StringBuffer on StringWriter, and BufferedInputStream on ByteArrayInputStream)
+ * 4) Demonstrate the use of the DataInputStream and DataOutputStream (DONE DataInputStream in readFile(), DataOutputStream in writeFile())
  */
 
-public class Exercise_03 {
+class Exercise_03 {
+    String s = "I'm using this string to demonstrate reading and/or writing to files";
+
+    public void readFile() {
+       try  (FileInputStream fIs = new FileInputStream("src/labs_examples/input_output/files/encryptionExample.txt");
+             DataInputStream dIs = new DataInputStream(fIs)) {
+
+           int i;
+           while ((i=dIs.read()) != -1){
+               System.out.print((char) i);
+           }
+
+       } catch (IOException ex){
+           System.out.println("Something went wrong with the DataInputStream");
+           ex.printStackTrace();
+       }
+
+    }
+    public void writeFile(){
+       try (FileOutputStream fOs = new FileOutputStream("src/labs_examples/input_output/files/dataOutputExample.txt");
+            DataOutputStream dOs = new DataOutputStream(fOs)) {
+
+            byte[] b = s.getBytes();
+            dOs.write(b);
+            dOs.flush();
+           System.out.println("Check dataOutputExample.txt to verify successful file write....");
+
+       } catch (Exception e){
+           System.out.println("Issue with DataOutputStream...");
+           e.printStackTrace();
+       }
+
+    }
     public static void main(String[] args) {
 
         byte[] bytes = new byte[50];
@@ -50,10 +82,10 @@ public class Exercise_03 {
         String str = "This is an example of using the StringReader character stream";
         int i = 0;
         try (StringReader s = new StringReader(str)) {
-            while((i = s.read()) != -1){
+            while ((i = s.read()) != -1) {
                 System.out.print((char) i);
             }
-        } catch (IOException exc){
+        } catch (IOException exc) {
             System.out.println("Problem with StringReader");
             exc.printStackTrace();
         }
@@ -71,6 +103,35 @@ public class Exercise_03 {
         System.out.println(output.toString());
         System.out.println("-----------------------------------------------------------------");
 
+        try (BufferedInputStream bIS = new BufferedInputStream(new ByteArrayInputStream(bytes))) {
+            for (byte b = 0; b < bytes.length / 2; b++) {
+                bytes[b] = b;
+            }
+
+            System.out.println("Bytes from input stream (first half): ");
+
+            for (int a = 0; a < bytes.length / 2; a++) {
+                a = bIS.read();
+                System.out.print(a + ", ");
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Something went wrong with BufferedInputStream");
+            ex.printStackTrace();
+        }
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------");
+
+        Exercise_03 dis = new Exercise_03();
+        dis.readFile();
+
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------");
+
+        dis.writeFile();
+
     }
 
 }
+
+
